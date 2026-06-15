e965712a3bfc243e440d9856c118a6394bd0c93b
Nick Remy <nremy@cloudflare.com>
2026-06-15 16:34:34 -0400

    added middleware for ga4 bot score

---
+++ functions/_middleware.js
@@ -0,0 +1,22 @@
+// functions/_middleware.js
+export async function onRequest({ request, next }) {
+  const response = await next();
+  
+  const contentType = response.headers.get('content-type') || '';
+  if (!contentType.includes('text/html')) return response;
+
+  const bm = request.cf?.botManagement ?? {};
+
+  const script = `<script>
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+  'cfBotScore': ${bm.score ?? 'null'},
+  'cfVerifiedBot': ${String(bm.verifiedBot ?? false)},
+  'cfDetectionIds': ${JSON.stringify(bm.detectionIds ?? [])}
+});
+</script>`;
+
+  return new HTMLRewriter()
+    .on('head', { element(el) { el.prepend(script, { html: true }); } })
+    .transform(response);
+}
