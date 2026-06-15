16f66e7714843f832dce1c9da98dcd113f720f24
Nick Remy <nremy@cloudflare.com>
2026-06-15 17:12:44 -0400

    Apped data layer variables earlier

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,12 +1,9 @@
-// functions/_middleware.js
 export async function onRequest({ request, next }) {
   const response = await next();
-  
   const contentType = response.headers.get('content-type') || '';
   if (!contentType.includes('text/html')) return response;
 
   const bm = request.cf?.botManagement ?? {};
-
   const script = `<script>
 window.dataLayer = window.dataLayer || [];
 window.dataLayer.push({
@@ -16,7 +13,15 @@ window.dataLayer.push({
 });
 </script>`;
 
+  let injected = false;
   return new HTMLRewriter()
-    .on('title', { element(el) { el.before(script, { html: true }); } })
+    .on('script', {
+      element(el) {
+        if (!injected) {
+          el.before(script, { html: true });
+          injected = true;
+        }
+      }
+    })
     .transform(response);
 }
