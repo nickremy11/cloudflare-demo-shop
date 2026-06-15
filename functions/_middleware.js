6e7d87dbae9a935730d209742dbf9ca8957650da
Nick Remy <nremy@cloudflare.com>
2026-06-15 19:39:50 -0400

    Use cookie injection

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,27 +1,10 @@
 export async function onRequest({ request, next }) {
   const response = await next();
-  const contentType = response.headers.get('content-type') || '';
-  if (!contentType.includes('text/html')) return response;
-
   const bm = request.cf?.botManagement ?? {};
-  const script = `<script>
-window.dataLayer = window.dataLayer || [];
-window.dataLayer.push({
-  'cfBotScore': ${bm.score ?? 'null'},
-  'cfVerifiedBot': ${String(bm.verifiedBot ?? false)},
-  'cfDetectionIds': ${JSON.stringify(bm.detectionIds ?? [])}
-});
-</script>`;
-
-  let injected = false;
-  return new HTMLRewriter()
-    .on('script', {
-      element(el) {
-        if (!injected) {
-          el.before(script, { html: true });
-          injected = true;
-        }
-      }
-    })
-    .transform(response);
+  
+  const newHeaders = new Headers(response.headers);
+  newHeaders.append('Set-Cookie', 
+    `_cfbm=${bm.score ?? ''},${bm.verifiedBot ?? false}; Path=/; SameSite=Strict; Secure`
+  );
+  return new Response(response.body, { status: response.status, headers: newHeaders });
 }
